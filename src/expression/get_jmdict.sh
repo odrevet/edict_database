fe77112d489d816d04a2475366a1f88bfaee2165
odrevet <odrevet@gmail.com>
2021-11-07 12:43:42 +0100

    update JMDICT download address

--- src/expression/get_jmdict.sh
+++ src/expression/get_jmdict.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget http://ftp.monash.edu/pub/nihongo/JMdict.gz --directory-prefix=data
+wget ftp://ftp.edrdg.org/pub/Nihongo//JMdict.gz --directory-prefix=data
 gunzip data/JMdict.gz
